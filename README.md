584622bd44234a6187c57f58baf29073bb017482
Lucky <shikang.xiao@lianbio.com>
2022-07-04 14:57:29 +0800

    init

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
-# Demo-H5
-个人封装的H5Demo
+# demo1
+
+## Project setup
+```
+npm install
+```
+
+### Compiles and hot-reloads for development
+```
+npm run serve
+```
+
+### Compiles and minifies for production
+```
+npm run build
+```
+
+### Customize configuration
+See [Configuration Reference](https://cli.vuejs.org/config/).
---
+++ src/main.js
@@ -0,0 +1,25 @@
+import Vue from 'vue'
+import App from './App'
+
+Vue.config.productionTip = false
+
+import {router,RouterMount} from './router/router.js'
+Vue.use(router)
+
+import uView from 'uview-ui'
+Vue.use(uView)
+
+App.mpType = 'app'
+
+const app = new Vue({
+  ...App
+})
+
+//v1.3.5起 H5端 你应该去除原有的app.$mount();使用路由自带的渲染方式
+// #ifdef H5
+	RouterMount(app,router,'#app')
+// #endif
+
+// #ifndef H5
+	app.$mount(); //为了兼容小程序及app端必须这样写才有效果
+// #endif
